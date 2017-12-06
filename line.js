059426dc979e836dc054f9be9940a8ffc0190267
AdamMusso <31658428+AdamMusso@users.noreply.github.com>
2017-12-06 14:32:21 +0100

    Add files via upload

---
+++ line.js
@@ -0,0 +1,69 @@
+Vue.component('line-chart', {
+
+    extends: VueChartJs.Line,
+
+    props: ['data', 'options'],
+
+    mounted() {
+
+        var data = this.data || {
+
+            labels: ['2005', '2006', '2007', '2008', '2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016'],
+
+            datasets: [
+
+                {
+
+                    label: 'GitHub Commits',
+
+                    borderColor: 'red',
+
+                    backgroundColor: 'black',
+
+                    data: [4, 3, 2, 5, 1, 1, 6, 2, 4, 4, 3, 7]
+
+}
+
+]
+
+        };
+
+
+
+        var options = this.options || {
+
+            legend: {
+
+                display: false
+
+            },
+
+            scales: {
+
+                yAxes: [
+
+                    {
+
+                        ticks: {
+
+                            min: 0,
+
+                            max: 10
+
+                        }
+
+}
+
+]
+
+            }
+
+        };
+
+
+
+        this.renderChart(data, options);
+
+    }
+
+});
